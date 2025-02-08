b54c99e60abef3e9f55ad2ee536f703a528052a0
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-08 11:27:05 -0800

    Updated chihuahua image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     return (
         <main className="grid h-screen grid-cols-2 grid-rows-1 gap-16 p-16">
             <div className="self-center justify-self-end">
-                <Image src="/chihuahua.svg" alt="Chihuahua" width={400} height={320} />
+                <Image src="/chihuahua.svg" alt="Chihuahua graphic" priority={true} width={400} height={320} />
             </div>
             <div className="flex flex-col gap-2 self-center justify-self-start">
                 <h1 className="text-5xl font-bold leading-none transition-font">fetch a friend</h1>
