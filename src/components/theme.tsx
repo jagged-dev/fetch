cb6dba00e109c58f38dccfe83e1f8df0796c1960
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-08 09:52:30 -0800

    Added theme component

---
+++ src/components/theme.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export default function Theme() {
+    const [icon, setIcon] = useState(<Moon />);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+                document.documentElement.classList.add("dark");
+                localStorage.theme = "dark";
+                setIcon(<Sun />);
+            } else {
+                document.documentElement.classList.remove("dark");
+                localStorage.theme = "light";
+                setIcon(<Moon />);
+            }
+        }
+    }, []);
+
+    function toggleTheme() {
+        document.documentElement.classList.toggle("dark");
+        localStorage.theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
+        setIcon(document.documentElement.classList.contains("dark") ? <Sun /> : <Moon />);
+    }
+
+    return <button onClick={toggleTheme}>{icon}</button>;
+}
