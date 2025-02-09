e5b6ebf4fef2dce6ec2f84cd36a15ba2bfe6869e
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-09 13:10:48 -0800

    Added responsivity to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import Footer from "@/components/footer";
 
 export default function Page() {
     return (
-        <main className="grid h-screen grid-cols-2 grid-rows-1 gap-16 p-16">
-            <div className="self-center justify-self-end">
-                <Image src="/chihuahua.svg" alt="Chihuahua graphic" width={400} height={320} priority={true} />
+        <main className="grid h-screen grid-cols-1 grid-rows-2 gap-8 p-8 xl:grid-cols-2 xl:grid-rows-1 xl:gap-16 xl:p-16">
+            <div className="self-end justify-self-center xl:self-center xl:justify-self-end">
+                <Image src="/chihuahua.svg" alt="Chihuahua graphic" height={200} width={240} priority={true} className="xl:h-[320px] xl:w-[400px]" />
             </div>
-            <div className="flex flex-col gap-2 self-center justify-self-start">
-                <h1 className="text-5xl font-bold leading-none transition-font">fetch a friend</h1>
-                <p className="text-xl text-gunmetal transition-font dark:text-silver">one fetch closer to your new best friend</p>
+            <div className="flex flex-col items-center gap-2 self-start justify-self-center xl:items-start xl:self-center xl:justify-self-start">
+                <h1 className="text-3xl font-bold leading-none transition-font xl:text-5xl">fetch a friend</h1>
+                <p className="text-gunmetal transition-font xl:text-xl dark:text-silver">one fetch closer to your new best friend</p>
                 <Link href="/login">
-                    <button className="mt-4 h-10 w-32 rounded-md bg-coal p-2 text-snow transition hover:translate-x-2 hover:bg-opacity-75 dark:bg-snow dark:text-coal">{`Get Started ->`}</button>
+                    <button className="mt-4 h-12 w-36 rounded-lg bg-coal p-2 text-snow transition hover:translate-x-2 hover:bg-opacity-75 dark:bg-snow dark:text-coal">{`Get Started ->`}</button>
                 </Link>
             </div>
-            <div className="col-span-2">
+            <div className="xl:col-span-2">
                 <Footer />
             </div>
         </main>
