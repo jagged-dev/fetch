189c93dea20345c154eb87a72889be0547f83463
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-09 21:18:22 -0800

    Added middleware file for authorization handling

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
+
+export default async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const cookieStore = await cookies();
+  const authorized = cookieStore.get("authorized")?.value === "true";
+
+  if (authorized && path.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!authorized && path.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
