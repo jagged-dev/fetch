6803948ca2b0fb183920deb04dd3c4e2cb139a90
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-10 22:44:37 -0800

    Updated leave component

--- src/components/leave.tsx
+++ src/components/leave.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import { useState } from "react";
 import { redirect } from "next/navigation";
-import { LogOut } from "lucide-react";
+import { LogOut, LoaderCircle } from "lucide-react";
 import { logOut } from "@/ts/api";
 
 export default function Leave() {
+    const [icon, setIcon] = useState(<LogOut />);
+
     async function leave() {
+        setIcon(<LoaderCircle className="animate-spin" />);
         await logOut();
         redirect("/");
     }
 
     return (
         <button onClick={leave} className="text-coal transition-font hover:text-red dark:text-snow">
-            <LogOut />
+            {icon}
         </button>
     );
 }
