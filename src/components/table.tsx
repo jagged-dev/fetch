cb41cebbaa5fab513eff33ea71d3d2425f603be9
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-10 18:16:23 -0800

    Added table component

---
+++ src/components/table.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Table() {
+    return (
+        <>
+            <div className="grow rounded-lg border border-gunmetal/25 p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50"></div>
+        </>
+    );
+}
