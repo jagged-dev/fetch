66ad147fdfe0910a2cc11c8f19415149d7dcdaf4
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-10 18:51:51 -0800

    Updated table component

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -3,7 +3,7 @@
 export default function Table() {
     return (
         <>
-            <div className="grow rounded-lg border border-gunmetal/25 p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50"></div>
+            <div className="h-full w-full rounded-lg border border-gunmetal/25 p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50"></div>
         </>
     );
 }
