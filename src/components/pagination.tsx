6b04bf3cb6706768b30f7269dd9f87b0186950bb
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-10 18:51:17 -0800

    Updated pagination component

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -3,7 +3,7 @@
 export default function Pagination() {
     return (
         <>
-            <div className="rounded-lg border border-gunmetal/25 p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50"></div>
+            <div className="w-full rounded-lg border border-gunmetal/25 p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50"></div>
         </>
     );
 }
