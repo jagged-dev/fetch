715814dd9feba2d8722019788e44967f2114445c
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-11 00:03:24 -0800

    Updated search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,9 +1,33 @@
 "use client";
 
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
 export default function Search() {
+    const params = useSearchParams();
+    const path = usePathname();
+    const router = useRouter();
+
+    const search = useDebouncedCallback((term) => {
+        const searchParams = new URLSearchParams(params);
+        searchParams.set("page", "1");
+        if (term) {
+            searchParams.set("query", term);
+        } else {
+            searchParams.delete("query");
+        }
+        router.replace(`${path}?${searchParams.toString()}`);
+    }, 300);
+
     return (
-        <>
-            <input type="text" placeholder="Search" onChange={(e) => {}} className="h-12 w-full rounded-lg border border-gunmetal/25 bg-snow p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:bg-coal dark:hover:border-silver/50" />
-        </>
+        <input
+            type="text"
+            placeholder="Search"
+            defaultValue={params.get("query")?.toString()}
+            onChange={(e) => {
+                search(e.target.value);
+            }}
+            className="h-12 w-full rounded-lg border border-gunmetal/25 bg-snow p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:bg-coal dark:hover:border-silver/50"
+        />
     );
 }
