11eaa1ae85ba9b3b5c444d1fb8c6a5d9a70f8588
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-10 18:09:45 -0800

    Added search component

---
+++ src/components/search.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Search() {
+    return (
+        <>
+            <input type="text" placeholder="Search" onChange={(e) => {}} className="h-12 w-full rounded-lg border border-gunmetal/25 bg-snow p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:bg-coal dark:hover:border-silver/50" />
+        </>
+    );
+}
