852a8b2715700be387054e85aa0faa55b2c79468
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-10 21:50:07 -0800

    Updated filter button

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -5,9 +5,9 @@ import { SlidersHorizontal } from "lucide-react";
 export default function Filter() {
     return (
         <button className="h-12 w-12 rounded-lg border border-gunmetal/25 p-2 transition hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50">
-            <div className="flex justify-center">
-                <SlidersHorizontal className="text-gunmetal transition-font dark:text-silver" />
-            </div>
+            <span className="flex justify-center text-gunmetal transition-font dark:text-silver">
+                <SlidersHorizontal />
+            </span>
         </button>
     );
 }
