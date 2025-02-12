6fb92da4665c16dd025a4003d50bdc7143d45a0e
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-11 21:29:56 -0800

    Updated dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,17 +13,14 @@ export default async function Page(props: { searchParams?: Promise<{ query?: str
     return (
         <div className="flex h-full items-center justify-center">
             <div className="flex h-full w-11/12 flex-col gap-2 xl:w-10/12">
-                <div className="flex">
-                    <div className="flex w-full gap-2 xl:w-1/3">
-                        <Search />
-                        <Filter />
-                    </div>
-                    <div className="hidden w-1/3 justify-center xl:flex">
-                        <Link href="/">
-                            <Image src="/samoyed.svg" alt="Samoyed graphic" height={120} width={180} priority={true} className="-mt-22 mb-4" />
-                        </Link>
-                    </div>
-                    <div className="hidden w-1/3 xl:flex"></div>
+                <div className="-mt-22 mb-4 hidden w-full justify-center xl:flex">
+                    <Link href="/">
+                        <Image src="/samoyed.svg" alt="Samoyed graphic" height={120} width={180} priority={true} />
+                    </Link>
+                </div>
+                <div className="flex w-full gap-2 xl:-mt-14 xl:w-1/3">
+                    <Search />
+                    <Filter />
                 </div>
                 <Table key={query + page} query={query} page={page} />
                 <Pagination />
