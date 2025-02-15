7f07e178ef2b6375e66185353b3cfbcb77698bd8
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-15 12:56:04 -0800

    Updated input styling

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -23,7 +23,7 @@ export default function Search() {
             onChange={(e) => {
                 search(e.target.value);
             }}
-            className="h-12 grow rounded-lg border border-gunmetal/25 bg-snow p-4 transition hover:border-gunmetal/50 dark:border-silver/25 dark:bg-coal dark:hover:border-silver/50"
+            className="h-12 grow rounded-lg border border-gunmetal/25 bg-transparent p-4 hover:border-gunmetal/50 dark:border-silver/25 dark:hover:border-silver/50"
         />
     );
 }
