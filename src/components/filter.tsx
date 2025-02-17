c560b5ca998b7ae412423615ab2ec0cc54cb3606
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2025-02-17 11:07:56 -0800

    Updated filter component

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -55,9 +55,9 @@ export default function Filter() {
             </button>
             <Dialog open={open} onClose={() => setOpen(false)}>
                 <DialogBackdrop transition className="fixed inset-0 bg-gunmetal/75 transition data-[closed]:opacity-0 dark:bg-silver/25" />
-                <div className="fixed inset-0 flex w-screen items-center justify-center p-8 xl:p-16">
-                    <DialogPanel transition className="flex h-full w-full flex-col gap-8 rounded-lg bg-snow p-8 transition data-[closed]:scale-95 data-[closed]:opacity-0 xl:p-16 dark:bg-coal">
-                        <DialogTitle className="text-3xl font-bold leading-none xl:text-5xl">Filters</DialogTitle>
+                <div className="fixed inset-0 flex w-screen items-center justify-center p-8">
+                    <DialogPanel transition className="flex h-full w-full flex-col gap-8 rounded-lg bg-snow p-8 transition data-[closed]:scale-95 data-[closed]:opacity-0 dark:bg-coal">
+                        <DialogTitle className="text-3xl font-bold leading-none">Filters</DialogTitle>
                         <button onClick={() => setShowFavorites(!showFavorites)} className="group flex w-fit items-center gap-2 rounded-lg border border-gunmetal/25 p-2 transition hover:border-pink dark:border-silver/25 dark:hover:border-pink">
                             <Heart className={clsx("transition group-hover:text-pink", { "fill-pink text-pink": showFavorites })} />
                             <p className="text-md leading-none">Favorites only</p>
